f6cef7b90b73ab1652d62fdab323c4f00170108f
Bekbolot Bekmamatov <bbekmamatov1991@gmail.com>
2022-02-17 19:49:51 +0600

    hw3

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,25 +1,35 @@
-from typing import ChainMap
 from django.db import models
-from django.forms import CharField
 
 class Director(models.Model):
     name = models.CharField(max_length=100)
 
+    @property
+    def movies_count(self):
+        return self.director.all().count()      
+
     def __str__(self):
-        return self.name
+        return self.name.director.all()
 
 class Movie(models.Model):
     title = models.CharField(max_length=100, null=True)
     description = models.TextField()
     duration = models.TimeField()
-    director = models.ForeignKey(Director, on_delete=models.DO_NOTHING)
+    director = models.ForeignKey(Director, on_delete=models.DO_NOTHING, related_name='director')
 
     def __str__(self):
         return self.title
 
 class Review(models.Model):
+    STARS = (
+        (1,'Bad'),
+        (2,'Nice'),
+        (3,'Good'),
+        (4,'Fine'),
+        (5,'Amazing'),
+    )
     text = models.TextField()
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='review')
+    stars = models.PositiveIntegerField(max_length=5, choices=STARS,null=True)
 
     def __str__(self):
         return f"Review: {self.movie.title}"
